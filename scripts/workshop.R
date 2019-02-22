7d5759c12e9a376d09992ae7a5a502167a16979d
Dr Peter Humburg <peter.humburg@mq.edu.au>
2019-02-22 11:04:24 +1100

    first plot

--- scripts/workshop.R
+++ scripts/workshop.R
@@ -3,3 +3,8 @@
 ## If you want to save plots you created, place them in the 'figures/' directory.
 ## You can create additional R files in the 'scripts/' directory.
 
+library(tidyverse)
+interviews_plotting <- read_csv("data_output/interviews_plotting.csv")
+
+interviews_plot <- ggplot(data = interviews_plotting, aes(x=no_membrs, y=number_items)) +
+  geom_jitter(alpha=0.5)
